edf3f8f11f112cf4dee398cb7a37f3aca3e8416d
tallal <tallal@gmail.com>
2020-06-30 22:41:54 +0100

    close files but check for errs

--- split.go
+++ split.go
@@ -40,7 +40,7 @@ func New() Splitter {
 func closeFile(f *os.File) {
 	err := f.Close()
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 }
